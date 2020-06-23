d7536f14796138f5262c62c971c971774312d141
Platt <lplatt@usgs.gov>
2020-06-23 14:55:57 -0500

    combine annotation and style steps

--- 2_process/src/process_and_style.R
+++ 2_process/src/process_and_style.R
@@ -7,15 +7,12 @@ combine_data <- function(...){
   return(nwis_data_clean)
 }
 
-annotate_data <- function(site_data_clean, site_filename){
+prepare_data_for_plot <- function(site_data_clean, site_filename){
   site_info <- read_csv(site_filename)
   annotated_data <- left_join(site_data_clean, site_info, by = "site_no") %>% 
     select(station_name = station_nm, site_no, dateTime, water_temperature, latitude = dec_lat_va, longitude = dec_long_va)
+  styled_data <- annotated_data %>% 
+    mutate(station_name = as.factor(station_name))
   
-  return(annotated_data)
-}
-
-
-style_data <- function(site_data_annotated){
-  mutate(site_data_annotated, station_name = as.factor(station_name))
+  return(styled_data)
 }
